f81e77739a89a196d068cb576815c11bbdfb8c68
Asif Ali Rizvan <fast.rizwaan@gmail.com>
2024-07-17 19:09:35 +0530

    updated app icon

--- winecharm.py
+++ winecharm.py
@@ -275,7 +275,7 @@ class WineCharmApp(Gtk.Application):
 
         app_icon_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
         app_icon_box.set_margin_start(10)
-        app_icon = Gtk.Image.new_from_icon_name("application-x-executable-symbolic")
+        app_icon = Gtk.Image.new_from_icon_name("io.github.fastrizwaan.WineCharm")
         app_icon_box.append(app_icon)
         self.headerbar.pack_start(app_icon_box)
 
