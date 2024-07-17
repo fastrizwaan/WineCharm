06a430a32dd845efe62dfdb7b04c2788734513bf
Asif Ali Rizvan <fast.rizwaan@gmail.com>
2024-07-17 08:24:40 +0530

    1st

---
+++ build.sh
@@ -0,0 +1,1 @@
+flatpak-builder --user --install --repo=repo --force-clean build-dir io.github.fastrizwaan.WineCharm.yaml ; flatpak kill io.github.fastrizwaan.WineCharm; flatpak run io.github.fastrizwaan.WineCharm
