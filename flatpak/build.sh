1625a9f44b62ec18543aa6aad20c0da06f2c1006
Asif Ali Rizvan <fast.rizwaan@gmail.com>
2024-10-12 19:50:54 +0530

    show debian install

--- flatpak/build.sh
+++ flatpak/build.sh
@@ -1,1 +1,2 @@
+echo "flatpak install org.gnome.Sdk/x86_64/47 org.gnome.Platform/x86_64/47 org.winehq.Wine//stable-24.08"
 flatpak-builder --user --install --repo=repo --force-clean build-dir io.github.fastrizwaan.WineCharm.yaml ; flatpak kill io.github.fastrizwaan.WineCharm; flatpak run io.github.fastrizwaan.WineCharm
