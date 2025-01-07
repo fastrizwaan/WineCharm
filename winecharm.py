99e49a5ebeb48655ce80f0762d4d626c6f6138f2
Asif Ali Rizvan <fast.rizwaan@gmail.com>
2025-01-08 01:18:09 +0530

    create bottle: move directory size check before suggesting tar.zst

--- winecharm.py
+++ winecharm.py
@@ -3645,7 +3645,7 @@ class WineCharmApp(Gtk.Application):
         find_replace_media_username = {f'/media/{user}/': '/media/%USERNAME%/'}
         restore_media_username = {'/media/%USERNAME%/': f'/media/{user}/'}
 
-                # Extract exe_file from script_data
+        # Extract exe_file from script_data
         script_data = self.extract_yaml_info(script_key)
         if not script_data:
             raise Exception("Script data not found.")
@@ -3657,19 +3657,6 @@ class WineCharmApp(Gtk.Application):
         game_dir = wineprefix / "drive_c" / "GAMEDIR"
         game_dir_exe = game_dir / exe_path.name / exe_name
         # Step 2: Define the steps for the backup process
-
-        # Calculate the directory size in bytes
-        directory_size = self.get_directory_size(exe_path)
-
-        # Convert directory size to GB for comparison
-        directory_size_gb = directory_size / (1024 ** 3)  # 1 GB is 1024^3 bytes
-        print("----------------------------------------------------------")
-        print(directory_size)
-        print(directory_size_gb)
-
-        if directory_size_gb > 3:
-            print("Size Greater than 3GB")
-        print("----------------------------------------------------------")
 
         def perform_backup_steps():
             steps = [
@@ -3722,7 +3709,7 @@ class WineCharmApp(Gtk.Application):
 
     def show_create_bottle_dialog(self, script, script_key, button):
 
-        # Step 0: Check if the executable file exists
+        # Step 1: Check if the executable file exists
         # Extract exe_file from script_data
         script_data = self.extract_yaml_info(script_key)
         if not script_data:
@@ -3731,13 +3718,30 @@ class WineCharmApp(Gtk.Application):
         exe_file = self.expand_and_resolve_path(script_data['exe_file'])
         #exe_file = Path(str(exe_file).replace("%USERNAME%", user))
         exe_path = exe_file.parent
+        exe_name = exe_file.name
+        game_dir = wineprefix / "drive_c" / "GAMEDIR"
+        game_dir_exe = game_dir / exe_path.name / exe_name
 
         # If exe_not found i.e., game_dir is not accessble due to unmounted directory
         if not exe_file.exists():
             GLib.timeout_add_seconds(0.5, self.show_info_dialog, "Exe Not Found", f"Not Mounted or Deleted?\n{exe_file}")
             return
 
-        # Step 1: Suggest the backup file name
+        # Step 2: Check for size if > 3GB ask the user:
+        # Calculate the directory size in bytes
+        directory_size = self.get_directory_size(exe_path)
+
+        # Convert directory size to GB for comparison
+        directory_size_gb = directory_size / (1024 ** 3)  # 1 GB is 1024^3 bytes
+        print("----------------------------------------------------------")
+        print(directory_size)
+        print(directory_size_gb)
+
+        if directory_size_gb > 3:
+            print("Size Greater than 3GB")
+        print("----------------------------------------------------------")
+
+        # Step 3: Suggest the backup file name
         default_backup_name = f"{script.stem} prefix backup.tar.zst"
 
         # Create a Gtk.FileDialog instance for saving the file
