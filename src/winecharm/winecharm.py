f34c28436d0157c21dba2bc131c28569955d0d4e
Asif Ali Rizvan <fast.rizwaan@gmail.com>
2025-06-22 05:14:06 +0530

    release 0.99.4

--- src/winecharm/winecharm.py
+++ src/winecharm/winecharm.py
@@ -91,7 +91,7 @@ class WineCharmApp(Adw.Application):
         
         # Move the global variables to instance attributes
         self.debug = False
-        self.version = "0.99.3"
+        self.version = "0.99.4"
         # Paths and directories
         self.winecharmdir = Path(os.path.expanduser("~/.var/app/io.github.fastrizwaan.WineCharm/data/winecharm")).resolve()
         self.prefixes_dir = self.winecharmdir / "Prefixes"
